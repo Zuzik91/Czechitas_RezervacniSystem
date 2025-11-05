a2915870ec1ef9ace9e8d9e06a26e2cc08dc268c
Zuzik91 <zuzana.smidova91@seznam.cz>
2025-11-05 20:07:04 +0100

    Lekce 6 - Odevzdání

--- src/tests/homework.spec.js
+++ src/tests/homework.spec.js
@@ -1,6 +1,8 @@
 import { test, expect } from '@playwright/test';
 import { afterEach, beforeEach } from 'node:test';
 
+require('dotenv').config();
+
 test('Homework-LOKATORY', async ({ page }) => {
 
     await page.goto('https://team8-2022brno.herokuapp.com/registrace');
@@ -249,6 +251,117 @@ test.describe('Homework - Lekce 5', { tag: "@smoke" }, () => {
             await heslo.fill('Heslo123');
             await expect(kontrolaHeslaVcetneTextu).toHaveText('Kontrola hesla');
             await kontrolaHesla.fill('Heslo123');
+            await tlacitkoZaregistrovat.click();
+            await expect(loginValueElement).toContainText(loginValue.slice(0, 5));
+            await expect(page.getByText('Přihlásit', { exact: true })).not.toBeVisible();
+        });
+    });
+});
+
+test.describe('Homework - Lekce 6', { tag: "@smoke" }, () => {
+
+    const { USER_NAME, USER_EMAIL, USER_PASSWORD } = process.env;
+
+    async function vyplnJmeno(page) {
+        const { USER_NAME } = process.env;
+        const jmenoPrijmeni = page.getByLabel('Jméno a příjmení');
+
+        await jmenoPrijmeni.fill(USER_NAME);
+    }
+
+    async function vyplnEmail(page) {
+        const { USER_EMAIL } = process.env;
+        const email = page.getByLabel('Email');
+
+        await email.fill(USER_EMAIL);
+    }
+
+    async function vyplnHeslo(page) {
+        const { USER_PASSWORD } = process.env;
+        const heslo = page.getByLabel('Heslo');
+
+        await heslo.fill(USER_PASSWORD);
+    }
+
+    async function kontrolaHeslaFun(page) {
+        const { USER_PASSWORD } = process.env;
+        const kontrolaHesla = page.getByLabel('Kontrola hesla');
+
+        await kontrolaHesla.fill(USER_PASSWORD);
+    }
+
+    //Načtení url
+    test.beforeEach(async ({ page }) => {
+        await page.goto('https://team8-2022brno.herokuapp.com/registrace');
+    });
+
+    test('Registrace s nevalidním heslem', { tag: "@negativ" }, async ({ page }) => {
+        const email = page.getByLabel('Email');
+        const tlacitkoZaregistrovat = page.getByRole('button', { name: 'Zaregistrovat' });
+
+        await vyplnJmeno(page);
+        await vyplnEmail(page);
+        await expect(page.getByRole('heading', { level: 1 })).toBeVisible();
+        await vyplnHeslo(page);
+        await expect(page.getByText('Přihlásit', { exact: true })).toBeEnabled();
+        await kontrolaHeslaFun(page);
+        await expect(page.getByAltText('Domů')).toBeVisible();
+        await tlacitkoZaregistrovat.click();
+        await expect(email).not.toBeEmpty();
+        await expect(page.getByText('Heslo musí obsahovat minimálně 6 znaků, velké i malé písmeno a číslici')).toBeVisible();
+    });
+
+    test('Registrace s již zaregistrovaným emailem', { tag: "@negativ" }, async ({ page }) => {
+        const jmenoPrijmeni = page.getByLabel('Jméno a příjmení');
+        const email = page.getByLabel('Email');
+        const heslo = page.getByLabel('Heslo');
+        const kontrolaHesla = page.getByLabel('Kontrola hesla');
+        const tlacitkoZaregistrovat = page.getByRole('button', { name: 'Zaregistrovat' });
+
+        await vyplnJmeno(page);
+        await expect.soft(jmenoPrijmeni).toHaveValue('Zuzana TESTER Šmídová');
+        await vyplnEmail(page);
+        await expect.soft(email).toHaveValue(/^mi.*/, { timeout: 15000 });
+        await heslo.fill(USER_PASSWORD+123);
+        await expect.soft(heslo).toHaveValue(/123/);
+        await kontrolaHesla.fill(USER_EMAIL+123);
+        await expect.soft(tlacitkoZaregistrovat).toHaveText('Zaregistrovat');
+        await tlacitkoZaregistrovat.click();
+        await expect(page.getByText('Účet s tímto emailem již existuje')).toBeVisible();
+    });
+
+    test.describe('Úspěšná registrace', { tag: "@smoke" }, () => {
+
+        //Odhlášení
+        test.afterEach(async ({ page }) => {
+            await page.getByRole('button', {name: /^Zuzana/}).click();
+            await page.getByText('Odhlásit').click();
+        });
+
+        test('Homework: validní registrace včetně asertace o úspěšném přihlášení - Lekce 4', { tag: "@happy path" }, async ({ page }) => {
+            const jmenoPrijmeni = page.getByLabel('Jméno a příjmení');
+            const email = page.getByLabel('Email');
+            const heslo = page.getByLabel('Heslo');
+            const kontrolaHesla = page.getByLabel('Kontrola hesla');
+            const kontrolaHeslaVcetneTextu = page.locator('xpath=//div[@class="form-group row"][4]');
+            const tlacitkoZaregistrovat = page.getByRole('button', { name: 'Zaregistrovat' });
+
+            const now = new Date().toISOString()
+                .replace(/[:.]/g, '-')
+                .replace('T', '_');
+            const emailValue = `${now}@milujiTe.cz`;
+
+            const loginValueElement = page.getByRole('button').filter({ has: page.locator('strong') });
+            const loginValue = USER_NAME;
+
+            await expect(jmenoPrijmeni).toBeVisible();
+            await jmenoPrijmeni.fill(loginValue);
+            await expect(email).toBeEditable();
+            await email.fill(emailValue);
+            await expect(heslo).toBeEnabled();
+            await heslo.fill(USER_PASSWORD+123);
+            await expect(kontrolaHeslaVcetneTextu).toHaveText('Kontrola hesla');
+            await kontrolaHesla.fill(USER_PASSWORD+123);
             await tlacitkoZaregistrovat.click();
             await expect(loginValueElement).toContainText(loginValue.slice(0, 5));
             await expect(page.getByText('Přihlásit', { exact: true })).not.toBeVisible();
