bdd910a98853d276518129a19e15ccc4b1122cb9
Zuzik91 <zuzana.smidova91@seznam.cz>
2025-11-25 09:25:36 +0100

    Lekce 8 - Odevzdání

--- src/tests/pages/login.page.js
+++ src/tests/pages/login.page.js
@@ -4,7 +4,7 @@ export class Login {
     constructor(page) {
         this.page = page;
         this.prihlasit = this.page.locator('xpath=//body//a[contains(., "Přihlásit")]');
-        this.tlacitkoZaregistrovat = this.page.locator('xpath=//a[contains(@class,"btn-secondary") and @href="https://team8-2022brno.herokuapp.com/registrace"]');
+        //this.tlacitkoZaregistrovat = this.page.locator('xpath=//a[contains(@class,"btn-secondary") and @href="https://team8-2022brno.herokuapp.com/registrace"]');
         this.nadpisPrihlaseni = this.page.getByRole('heading', { name: 'Přihlášení' });
     }
 
@@ -15,10 +15,8 @@ export class Login {
 
     async zaregistrujSe() {
         await this.prihlasit.click();
-        await this.nadpisPrihlaseni.waitFor({ state: 'visible' });
-        await this.tlacitkoZaregistrovat.waitFor({ state: 'visible' });
-        console.log(await this.page.getByRole('link', { name: 'Zaregistrujte se' }).count());
-        console.log(await this.tlacitkoZaregistrovat.isVisible());
-        await this.tlacitkoZaregistrovat.click();
+        await this.nadpisPrihlaseni.waitFor({ state: 'visible' })
+        await this.page.locator('xpath=//a[contains(@class,"btn-secondary") and @href="https://team8-2022brno.herokuapp.com/registrace"]').scrollIntoViewIfNeeded();
+        await this.page.locator('xpath=//a[contains(@class,"btn-secondary") and @href="https://team8-2022brno.herokuapp.com/registrace"]').click();
     }
 }
