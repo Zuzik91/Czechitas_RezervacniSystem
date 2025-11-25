58b748205a789632e0fd4212b6144daf7131bdff
Zuzik91 <zuzana.smidova91@seznam.cz>
2025-11-25 08:55:44 +0100

    Error - lokátor "Zaregistrujte se"

--- src/tests/pages/login.page.js
+++ src/tests/pages/login.page.js
@@ -4,7 +4,7 @@ export class Login {
     constructor(page) {
         this.page = page;
         this.prihlasit = this.page.locator('xpath=//body//a[contains(., "Přihlásit")]');
-        this.tlacitkoZaregistrovat = this.page.locator('xpath=//body//a[contains(., "Zaregistrujte se")]');
+        this.tlacitkoZaregistrovat = this.page.locator('xpath=//a[contains(@class,"btn-secondary") and @href="https://team8-2022brno.herokuapp.com/registrace"]');
         this.nadpisPrihlaseni = this.page.getByRole('heading', { name: 'Přihlášení' });
     }
 
@@ -16,6 +16,9 @@ export class Login {
     async zaregistrujSe() {
         await this.prihlasit.click();
         await this.nadpisPrihlaseni.waitFor({ state: 'visible' });
+        await this.tlacitkoZaregistrovat.waitFor({ state: 'visible' });
+        console.log(await this.page.getByRole('link', { name: 'Zaregistrujte se' }).count());
+        console.log(await this.tlacitkoZaregistrovat.isVisible());
         await this.tlacitkoZaregistrovat.click();
     }
 }
